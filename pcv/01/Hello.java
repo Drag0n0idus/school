22b12631263ce88f5338fffd074f81f12a87e6f6
vyt2-123456789 <vyt2.7@sspu-opava.cz>
2017-10-02 08:14:57 +0200

    added some shit

--- pcv/01/Hello.java
+++ pcv/01/Hello.java
@@ -1,7 +1,8 @@
-public class hello {
-    public static void main(String[] args){
+public class Hello {
+    public static void main(String[] args) {
         System.out.println("Ciselne soustavy");
+        System.out.println("----------------");
         int x = Integer.parseInt(args[0]);
-        System.out.format("HEX: %x%n",x);
+        System.out.format("HEX: %x%n", x);
     }
 }
